d22ff3c9613f3b2b18831a8f69a5d9549c8cda6b
Nehal Hasnayeen <searching.nehal@gmail.com>
2016-10-01 14:19:26 +0600

    dashboard sidebar style fixed

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -4,14 +4,17 @@
 
 @section('content')
 
-<div class="row">
-    
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-        @include('partials.sidebar')
-    </div>
+<div class="container">
+    <div class="row">
 
-    <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+        <div class="col-md-3 col-lg-3">
+            @include('partials.sidebar')
+        </div>
 
+        <div class="col-md-6 col-lg-6">
+
+        </div>
     </div>
+</div>
 
 @endsection
