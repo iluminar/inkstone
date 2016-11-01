40d0da2602786089147082170a064a504121a263
Nehal Hasnayeen <searching.nehal@gmail.com>
2016-11-02 01:23:58 +0600

    domain removed from route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -54,7 +54,6 @@ class RouteServiceProvider extends ServiceProvider
         Route::group([
             'middleware' => 'web',
             'namespace' => $this->namespace,
-            'domain' => 'iluminar.com',
         ], function ($router) {
             require base_path('routes/web.php');
         });
